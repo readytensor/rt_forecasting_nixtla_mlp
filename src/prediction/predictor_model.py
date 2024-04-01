1f1ebae3c8fff778751cbd7ee867686b9a4b7e15
moo3030 <mohamedabdelhamid3030@gmail.com>
2024-04-01 17:37:40 +0200

    put constraint on window size

--- src/prediction/predictor_model.py
+++ src/prediction/predictor_model.py
@@ -75,7 +75,11 @@ class Forecaster:
             "max_steps": 100,
             "learning_rate": trial.suggest_loguniform("learning_rate", 1e-5, 1e-1),
             "num_layers": trial.suggest_int("num_layers", 1, 5),
-            "hidden_size": trial.suggest_int("hidden_size", 10, 100),
+            "hidden_size": trial.suggest_int(
+                "hidden_size",
+                data_schema.forecast_length * 2,
+                data_schema.forecast_length * 5,
+            ),
             "num_lr_decays": 2,
             "input_size": trial.suggest_int("input_size", 5, 100),
             "random_seed": self.random_state,
